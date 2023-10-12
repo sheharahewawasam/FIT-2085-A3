d35d2e2b5459439d348472984641f6dbdca15ff1
sheharahewawasam <126374930+sheharahewawasam@users.noreply.github.com>
2023-10-12 18:13:06 +0800

    Update  mode2.py
    added the loops for the pirates and then the loops for the island, with the pirates score adding to a list

--- mode2.py
+++ mode2.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from random import random
+
 from island import Island
 
 class Mode2Navigator:
@@ -9,16 +13,35 @@ class Mode2Navigator:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.sea_island = []
+        self.n_pirates = n_pirates
 
     def add_islands(self, islands: list[Island]):
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.sea_island = islands
+
 
     def simulate_day(self, crew: int) -> list[tuple[Island|None, int]]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        pirates_count = self.n_pirates
+        score = []
+        for pirate in range(pirates_count):
+            total_money = 0
+            left_crew = 100
+            money_list = []
+            # first island
+            for island in self.sea_island :
+                random_number = random.randint(0, 100)
+                moneyCollected = min((random_number*island.money/island.marines),island.money)
+                total_money += moneyCollected
+                money_list.append((island,moneyCollected))
+                left_crew -= random_number
+            score.append((2*left_crew)+total_money)
+
+
+
+
